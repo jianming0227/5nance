031a942e51f71143c0aee2ff141d421ef8cf17bb
huishann915 <huishan040915@gmail.com>
2025-06-07 00:08:39 +0800

    send form to email

---
+++ frontend/js/contact.js
@@ -0,0 +1,9 @@
+function sendMail(){
+    let params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        feedback: document.getElementById("feedback").value,
+    }
+
+    emailjs.send("service_zyojtkc", "template_fdyhu63", params).then(alert("Email Sent!"));
+}
